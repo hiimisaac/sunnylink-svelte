267d4c2ea1ef77d22c14e31d49ebc22691bd0b66
Isaac Barham <development@isaacbarham.com>
2025-04-01 20:26:37 -0400

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,116 @@
+<div class="bg-base-200 min-h-screen">
+	<div class="hero bg-base-200">
+		<div class="hero-content text-center">
+			<div class="max-w-md lg:max-w-lg">
+				<h1 class="text-6xl font-semibold">Enjoy driving like never before</h1>
+				<p class="py-6">
+					Unparalleled development principles. Stability beyond limits. Community-driven always.
+				</p>
+				<button class="btn btn-primary">Get Started</button>
+			</div>
+		</div>
+	</div>
+	<div class="mx-auto px-15 py-6 xl:max-w-3/4">
+		<div class="mockup-browser border-base-300 mx-auto rounded-3xl border shadow-2xl">
+			<div class="mockup-browser-toolbar">
+				<div class="input">https://github.com/sunnypilot/sunnypilot</div>
+			</div>
+			<div class="bg-base-100 grid place-content-center py-6">
+				<div class="stats py-6 shadow">
+					<div class="stat">
+						<div class="stat-figure text-primary"></div>
+						<div class="stat-title">Total Miles</div>
+						<div class="stat-value text-primary">1.52K</div>
+						<div class="stat-desc">25% Higher Than Other sunnypilot Users</div>
+					</div>
+
+					<div class="stat">
+						<div class="stat-title">Total Drives</div>
+						<div class="stat-value text-secondary">2.6k</div>
+						<div class="stat-desc">21% more than last month</div>
+					</div>
+
+					<div class="stat">
+						<div class="stat-figure text-secondary">
+							<div class="avatar online">
+								<div class="w-16 rounded-full">
+									<img
+										src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+										alt="temp"
+									/>
+								</div>
+							</div>
+						</div>
+						<div class="stat-value">86%</div>
+						<div class="stat-title">Engagement Rating</div>
+						<div class="stat-desc text-secondary">23% more than last month</div>
+					</div>
+				</div>
+				<div class="flex py-6">
+					<div class="p-2">
+						<div class="card bg-base-100 px-2 shadow-sm">
+							<div class="card-body">
+								<div class="flex w-52 flex-col gap-4">
+									<div class="skeleton h-32 w-full"></div>
+									<div class="skeleton h-4 w-28"></div>
+									<div class="skeleton h-4 w-full"></div>
+									<div class="skeleton h-4 w-full"></div>
+								</div>
+								<progress class="progress progress-primary w-56" value="19" max="100"></progress>
+								<progress class="progress progress-primary w-56" value="33" max="100"></progress>
+								<progress class="progress progress-primary w-56" value="100" max="100"></progress>
+							</div>
+						</div>
+					</div>
+
+					<div class="p-2">
+						<div class="card bg-base-100 px-2 shadow-sm">
+							<div class="card-body">
+								<div class="flex w-52 flex-col gap-4">
+									<div class="skeleton h-32 w-full"></div>
+									<div class="skeleton h-4 w-28"></div>
+									<div class="skeleton h-4 w-full"></div>
+									<div class="skeleton h-4 w-full"></div>
+								</div>
+								<progress class="progress progress-primary w-56" value="22" max="100"></progress>
+								<progress class="progress progress-primary w-56" value="98" max="100"></progress>
+								<progress class="progress progress-primary w-56" value="100" max="100"></progress>
+							</div>
+						</div>
+					</div>
+					<div class="p-2">
+						<div class="card bg-base-100 px-2 shadow-sm">
+							<div class="card-body">
+								<div class="flex w-52 flex-col gap-4">
+									<div class="skeleton h-32 w-full"></div>
+									<div class="skeleton h-4 w-28"></div>
+									<div class="skeleton h-4 w-full"></div>
+									<div class="skeleton h-4 w-full"></div>
+								</div>
+								<progress class="progress progress-primary w-56" value="62" max="100"></progress>
+								<progress class="progress progress-primary w-56" value="15" max="100"></progress>
+								<progress class="progress progress-primary w-56" value="100" max="100"></progress>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="mx-auto w-full place-content-center py-6">
+		<div class="flex place-content-center">
+			<div class="badge badge-soft badge-success">Features</div>
+		</div>
+	</div>
+	<div class="grid w-full grid-cols-3 place-items-center py-5">
+		<div class="mockup-code bg-base text-primary-content w-3/4">
+			<pre data-prefix="$"><code>can be any color!</code></pre>
+		</div>
+		<div class="mockup-code w-3/4">
+			<pre data-prefix="$"><code>npm i daisyui</code></pre>
+		</div>
+		<div class="mockup-code w-3/4">
+			<pre data-prefix="$"><code>npm i daisyui</code></pre>
+		</div>
+	</div>
+</div>
