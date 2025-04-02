81de16733f44b9e085fc776006da7315046be0ee
Not Bruce Wayne <isaac@coffeecakestudios.com>
2025-04-01 23:53:11 -0400

    Safety

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import img from '$lib/images/smiling-astro.png';
+</script>
+
 <div class="bg-base-200 min-h-screen">
 	<div class="hero bg-base-200">
 		<div class="hero-content text-center">
@@ -99,18 +103,230 @@
 	</div>
 	<div class="mx-auto w-full place-content-center py-6">
 		<div class="flex place-content-center">
-			<div class="badge badge-soft badge-success">Features</div>
+			<div class="badge badge-soft badge-success badge-xl">Features</div>
 		</div>
 	</div>
-	<div class="grid w-full grid-cols-3 place-items-center py-5">
-		<div class="mockup-code bg-base text-primary-content w-3/4">
-			<pre data-prefix="$"><code>can be any color!</code></pre>
+	<div class="mx-auto grid w-full grid-cols-3 place-items-center justify-center xl:max-w-3/5">
+		<div class="card bg-base-100 w-96 shadow-sm">
+			<figure class="px-10 pt-10">
+				<img
+					src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+					alt="Shoes"
+					class="rounded-xl"
+				/>
+			</figure>
+			<div class="card-body items-center text-center">
+				<h2 class="card-title">Dynamic Experimental Control</h2>
+				<p>
+					Intelligently shift between ACC and Experimental Mode for the smoothest longitudinal
+					control. Enhanced stopping for things like red lights and stop signs while maintaining
+					your set speed in most other scenarios!
+				</p>
+				<div class="card-actions"></div>
+			</div>
 		</div>
-		<div class="mockup-code w-3/4">
-			<pre data-prefix="$"><code>npm i daisyui</code></pre>
+		<div class="card bg-base-100 w-96 shadow-sm">
+			<figure class="px-10 pt-10">
+				<img
+					src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+					alt="Shoes"
+					class="rounded-xl"
+				/>
+			</figure>
+			<div class="card-body items-center text-center">
+				<h2 class="card-title">MADS</h2>
+				<p>
+					Lateral Control and Longitudinal Control are able to be engaged/disengaged independently
+					of one another! This allows your vehicle to steer while you control the gas/brake or your
+					vehicle can control gas/break while you steer!
+				</p>
+				<div class="card-actions"></div>
+			</div>
 		</div>
-		<div class="mockup-code w-3/4">
-			<pre data-prefix="$"><code>npm i daisyui</code></pre>
+		<div class="card bg-base-100 w-96 shadow-sm">
+			<figure class="px-10 pt-10">
+				<img
+					src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+					alt="Shoes"
+					class="rounded-xl"
+				/>
+			</figure>
+			<div class="card-body items-center text-center">
+				<h2 class="card-title">Forced Car Recognition</h2>
+				<p>
+					Avoid the headaches of new ECU firmware versions after recalls and software OTAs! Force
+					sunnypilot to recognize your vehicle everytime you start sunnypilot. This allows you to
+					bypass those mornings where your car is not recognized due to an OTA
+				</p>
+				<div class="card-actions"></div>
+			</div>
+		</div>
+	</div>
+	<div class="mx-auto w-full place-content-center pt-15">
+		<div class="flex place-content-center">
+			<div class="badge badge-soft badge-info badge-xl">Branch Info</div>
+		</div>
+	</div>
+	<div class="mx-auto w-full place-content-center py-6 xl:max-w-2/3">
+		<div class="tabs tabs-box">
+			<input type="radio" name="my_tabs_6" class="tab" aria-label="Release" checked />
+			<div class="tab-content bg-base-100 border-base-300 p-6">
+				<div class="mockup-window bg-base-100 border-base-300 border">
+					<div class="grid h-80 px-10">
+						<h1 class="text-3xl font-bold">Why use release?</h1>
+						<ol class="list-inside list-decimal">
+							<li>
+								Stability! The Release branch is our most consistent, with a nonzero probability of
+								introducing regressions. We've spent considerable resources developing a strong
+								suite of CI tests to ensure the best for all of our users
+							</li>
+							<li class="pt-2">
+								If a regression does ship, it's all hands on deck! Release regressions are rare, but
+								we're not perfect. When we do ship these, we ensure that <span class="font-bold"
+									>someone</span
+								> addresses these issues immediately.
+							</li>
+							<li class="pt-2">
+								Stress-free! Experience stress-free updates with our release branch—each feature is
+								thoroughly verified by trusted testers and our community, ensuring it's ready for
+								immediate use without sacrificing stability.
+							</li>
+						</ol>
+						<p class="pt-4">Ready to experience the most stable sunnypilot version?</p>
+						<p>
+							On your Comma 3X device, select &quot;Custom Software&quot; and input <code
+								>sunnypilot/release-c3-new</code
+							>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<input type="radio" name="my_tabs_6" class="tab" aria-label="Staging" />
+			<div class="tab-content bg-base-100 border-base-300 p-6">
+				<div class="mockup-window bg-base-100 border-base-300 border">
+					<div class="grid h-80 px-10">
+						<h1 class="text-3xl font-bold">...then what's staging for?</h1>
+						<ol class="list-inside list-decimal">
+							<li>
+								Cautious Adventurers! The staging branch gives you a sneak peek at upcoming features
+								that have undergone preliminary testing. It's for those who want to explore new
+								innovations early, knowing they&apos;re still getting refined.
+							</li>
+							<li class="pt-2">
+								Test-Drive Innovation! Experience early updates in our staging environment—each new
+								feature gets an initial round of testing by a group of trusted testers and community
+								members. This lets you explore fresh functionality while helping us catch and
+								resolve any issues before the final release.
+							</li>
+						</ol>
+						<p class="pt-4">Ready to experience near bleeding-edge sunnypilot version?</p>
+						<p>
+							On your Comma 3X device, select &quot;Custom Software&quot; and input <code
+								>sunnypilot/staging-c3-new</code
+							>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<input type="radio" name="my_tabs_6" class="tab" aria-label="Dev" />
+			<div class="tab-content bg-base-100 border-base-300 p-6">
+				<div class="mockup-window bg-base-100 border-base-300 border">
+					<div class="grid h-80 px-10">
+						<h1 class="text-3xl font-bold">Wait! Why dev then?</h1>
+						<ol class="list-inside list-decimal">
+							<li>
+								ABSOLUTE MADMEN! The dev branch is our playground. We develop new features and ship
+								often. We break things sometimes (YOU HAVE TO BREAK EGGS TO MAKE AN OMELTTE), but
+								we're pretty adamant on trying not to do that. We ship new features as often as we
+								can and refine existing ones constantly.
+							</li>
+							<li class="pt-2">
+								You want to help shape <span class="font-bold">sunnypilot</span>! We take community
+								feedback as serious as internal feedback. A lot of features, improvements and
+								quality-of-life upgrades have stemmed from discussions with your community members.
+								This allows you to drive the ship and help us identify where we should put our
+								resources!
+							</li>
+						</ol>
+						<p class="pt-4">Ready to experience the most fun sunnypilot version?</p>
+						<p>
+							On your Comma 3X device, select &quot;Custom Software&quot; and input <code
+								>sunnypilot/dev-c3-new</code
+							>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="mx-auto w-full place-content-center pt-15">
+		<div class="flex place-content-center">
+			<div class="badge badge-soft badge-primary badge-xl">FAQ</div>
+		</div>
+	</div>
+	<div class="mx-auto grid grid-cols-2 xl:max-w-3/5">
+		<img src={img} alt="astronaut" class="h-1/2 bg-transparent" />
+
+		<div class="p-6">
+			<details class="bg-base-100 border-base-300 collapse border">
+				<summary class="collapse-title font-semibold">What is sunnypilot?</summary>
+				<div class="collapse-content text-sm">
+					sunnypilot is an open-source driving assistance software designed to enhance the driving
+					experience by providing features like Modified Assistive Driving Safety (MADS), Forced Car
+					Recognition (FCR), Dynamic Experimental Control (DEC) and many other quality-of-life
+					features
+				</div>
+			</details>
+			<details class="bg-base-100 border-base-300 collapse mt-2 border">
+				<summary class="collapse-title font-semibold"
+					>Which driving models are best for heavy traffic?
+				</summary>
+				<div class="collapse-content text-sm">
+					Well, it depends. This is personal preference so we recommend trying a vareity of models.
+					However, Filet o' Fish has been regarded as one of the better models as of late and is
+					regarded as much better at longitudinal control than previous models so maybe start with
+					this one.
+				</div>
+			</details>
+			<details class="bg-base-100 border-base-300 collapse mt-2 border">
+				<summary class="collapse-title font-semibold"
+					>How do I leave feedback for the sunnypilot team?
+				</summary>
+				<div class="collapse-content text-sm">
+					You can leave feedback by joining the official sunnypilot <a
+						class="text-blue-500"
+						href="https://discord.gg/7wq8zuxB">Discord</a
+					> server. Feel free to ping any of the moderators or any of the devs on the team!
+				</div>
+			</details>
+			<details class="bg-base-100 border-base-300 collapse mt-2 border">
+				<summary class="collapse-title font-semibold"
+					>Where can I find documentation and support for sunnypilot?
+				</summary>
+				<div class="collapse-content text-sm">
+					The best place to find documentation and support will be the official sunnypilot <a
+						class="text-blue-500"
+						href="https://discord.gg/7wq8zuxB">Discord</a
+					> server. There will be better places in the not-so-distant future but for now, this is the
+					best place. We have a community willing and able to provide support and for anything they can't
+					answer, our incredibly talented dev team is happy to help.
+				</div>
+			</details>
+			<details class="bg-base-100 border-base-300 collapse mt-2 border">
+				<summary class="collapse-title font-semibold"
+					>There's a lot of settings. Where do I get started?</summary
+				>
+				<div class="collapse-content text-sm">
+					There is a lot of settings! We've done a lot of work to try to curate our settings to a
+					wide audience so it may seem overwhelming at first. We suggest installing sunnypilot and
+					ensuring it's enabled. Drive like this for a few weeks and then start going through the
+					settings again after knowing how stock sunnypilot drives. From there, if you tap each
+					setting a description will be displayed! We're working on bringing back our online portal
+					for setting your preferences from device, so be on the lookout!
+				</div>
+			</details>
 		</div>
 	</div>
 </div>
