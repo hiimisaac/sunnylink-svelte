a6ded8fb1460ebbdc69e2a6f3b4910a17b7588d7
Isaac Barham <development@isaacbarham.com>
2025-04-13 22:47:52 -0400

    istg

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,18 @@
 import { env } from '$env/dynamic/private';
 import type { Actions } from './$types';
-import { Buffer } from 'node:buffer';
-
-globalThis.Buffer = Buffer;
 
 export const actions: Actions = {
 	signIn: async ({ locals }) => {
-		// Or use the following code to redirect after the callback is done.
 		await locals.logtoClient.signIn({
 			redirectUri: env.LOGTO_REDIRECT,
-			postRedirectUri: env.LOGTO_ENDPOINT
+			postRedirectUri: '/'
 		});
 	},
 	signOut: async ({ locals }) => {
-		await locals.logtoClient.signOut('http://localhost:5173/');
+		const baseUrl =
+			env.NODE_ENV === 'production'
+				? 'https://sunnylink-svelte.netlify.app/'
+				: 'http://localhost:5173/';
+		await locals.logtoClient.signOut(baseUrl);
 	}
 };
