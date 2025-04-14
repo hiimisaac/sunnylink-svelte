31a908c43d659bf0f35050c95170aec4e6f03aa4
Isaac Barham <development@isaacbarham.com>
2025-04-13 23:19:57 -0400

    idk

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-netlify';
 
 const config = {
 	preprocess: vitePreprocess(),
