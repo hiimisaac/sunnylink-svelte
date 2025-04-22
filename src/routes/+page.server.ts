5404eadacaa62c64b050e90d11debcfc7b9ad771
Isaac Barham <development@isaacbarham.com>
2025-04-22 10:43:44 -0400

    plz sr

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,16 @@
-import { LOGTO_REDIRECT, NODE_ENV } from '$env/dynamic/private';
+import { env } from '$env/dynamic/private';
 import { Actions } from '../../.svelte-kit/types/src/routes/$types.d.ts';
 
 export const actions: Actions = {
 	signIn: async ({ locals }) => {
 		await locals.logtoClient.signIn({
-			redirectUri: LOGTO_REDIRECT,
+			redirectUri: env.LOGTO_REDIRECT,
 			postRedirectUri: '/'
 		});
 	},
 	signOut: async ({ locals }) => {
-		const baseUrl =
-			NODE_ENV === 'production'
-				? 'https://sunnylink-svelte.netlify.app/'
-				: 'http://localhost:5173/';
+		const baseUrl = 'https://sunnylink-svelte.netlify.app/';
+
 		await locals.logtoClient.signOut(baseUrl);
 	}
 };
