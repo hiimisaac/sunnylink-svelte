b1c9d60c44fdbfdb2cc97249b4c3e5b692af84b0
Isaac Barham <development@isaacbarham.com>
2025-04-22 11:26:19 -0400

    i dont get it

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,14 +2,10 @@ import { Actions } from '../../.svelte-kit/types/src/routes/$types.d.ts';
 
 export const actions: Actions = {
 	signIn: async ({ locals }) => {
-		try {
-			await locals.logtoClient.signIn({
-				redirectUri: 'https://sunnylink-svelte.netlify.app/',
-				postRedirectUri: '/'
-			});
-		} catch (error) {
-			console.log(error);
-		}
+		await locals.logtoClient.signIn({
+			redirectUri: '/',
+			postRedirectUri: '/'
+		});
 	},
 	signOut: async ({ locals }) => {
 		const baseUrl = 'https://sunnylink-svelte.netlify.app/';
