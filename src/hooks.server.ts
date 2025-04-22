3fc90f79c7353c297fdad282b24393c3848f7508
Isaac Barham <development@isaacbarham.com>
2025-04-22 11:01:26 -0400

    idk anymore

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,14 @@
 import { handleLogto, UserScope } from '@logto/sveltekit';
-import {
-	LOGTO_ENDPOINT,
-	LOGTO_APP_ID,
-	LOGTO_APP_SECRET,
-	LOGTO_COOKIE_ENCRYPTION_KEY
-} from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export const handle = handleLogto(
 	{
-		endpoint: LOGTO_ENDPOINT ?? '',
-		appId: LOGTO_APP_ID ?? '',
-		appSecret: LOGTO_APP_SECRET,
+		endpoint: env.LOGTO_ENDPOINT ?? '',
+		appId: env.LOGTO_APP_ID ?? '',
+		appSecret: env.LOGTO_APP_SECRET,
 		scopes: [UserScope.Identities]
 	},
 	{
-		encryptionKey: LOGTO_COOKIE_ENCRYPTION_KEY ?? ''
+		encryptionKey: env.LOGTO_COOKIE_ENCRYPTION_KEY ?? ''
 	}
 );
