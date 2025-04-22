c3da46d2beea7b9367ef8e07adb2db80fde5e99a
Isaac Barham <development@isaacbarham.com>
2025-04-22 11:19:22 -0400

    Testing

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,10 @@
-import { env } from '$env/dynamic/private';
 import { Actions } from '../../.svelte-kit/types/src/routes/$types.d.ts';
 
 export const actions: Actions = {
 	signIn: async ({ locals }) => {
-		console.log(env.LOGTO_COOKIE_ENCRYPTION_KEY);
 		try {
 			await locals.logtoClient.signIn({
-				redirectUri: env.LOGTO_REDIRECT,
+				redirectUri: 'https://sunnylink-svelte.netlify.app/',
 				postRedirectUri: '/'
 			});
 		} catch (error) {
