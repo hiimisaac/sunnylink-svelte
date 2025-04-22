30b05203a01724cbdb48d16d9659a7e082b12701
Isaac Barham <development@isaacbarham.com>
2025-04-22 11:16:06 -0400

    PLEASE

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,12 +3,15 @@ import { Actions } from '../../.svelte-kit/types/src/routes/$types.d.ts';
 
 export const actions: Actions = {
 	signIn: async ({ locals }) => {
-		console.log(env.LOGTO_REDIRECT);
-		console.log(env.LOGTO_APP_ID);
-		await locals.logtoClient.signIn({
-			redirectUri: env.LOGTO_REDIRECT,
-			postRedirectUri: '/'
-		});
+		console.log(env.LOGTO_COOKIE_ENCRYPTION_KEY);
+		try {
+			await locals.logtoClient.signIn({
+				redirectUri: env.LOGTO_REDIRECT,
+				postRedirectUri: '/'
+			});
+		} catch (error) {
+			console.log(error);
+		}
 	},
 	signOut: async ({ locals }) => {
 		const baseUrl = 'https://sunnylink-svelte.netlify.app/';
