219dbebc8b18b31c7ea6c4dab972a1b986946f0f
Isaac Barham <development@isaacbarham.com>
2025-04-22 11:03:17 -0400

    doubtful

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,8 @@ import { Actions } from '../../.svelte-kit/types/src/routes/$types.d.ts';
 
 export const actions: Actions = {
 	signIn: async ({ locals }) => {
+		console.log(env.LOGTO_REDIRECT);
+		console.log(env.LOGTO_APP_ID);
 		await locals.logtoClient.signIn({
 			redirectUri: env.LOGTO_REDIRECT,
 			postRedirectUri: '/'
