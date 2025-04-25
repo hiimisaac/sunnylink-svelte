248b37eee24d14a2cbef611e132ed8e8814fa322
Isaac Barham <development@isaacbarham.com>
2025-04-25 12:24:28 -0400

    Fixed some links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -195,9 +195,8 @@
 	<nav>
 		<h6 class="footer-title">Company</h6>
 		<a class="link link-hover">About us</a>
-		<a class="link link-hover">Contact</a>
-		<a class="link link-hover">Github</a>
-		<a class="link link-hover">Discord</a>
+		<a class="link link-hover" href="https://github.com/sunnypilot/sunnypilot">Github</a>
+		<a class="link link-hover" href="https://discord.gg/sunnypilot">Discord</a>
 	</nav>
 	<nav>
 		<h6 class="footer-title">Legal</h6>
