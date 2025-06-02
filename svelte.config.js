94236a969ef7e400b205aa42fef5dc8f40fb5e96
Isaac Barham <development@isaacbarham.com>
2025-06-02 08:41:28 -0400

    Woops

--- svelte.config.js
+++ svelte.config.js
@@ -7,11 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-<<<<<<< HEAD
-			base: '' // Changed to empty string for static deployments
-=======
-			base: 'sunnylink-svelte'
->>>>>>> dc63fe9 (Okay let's see)
+			base: '/sunnylink-svelte'
 		}
 	}
 };
