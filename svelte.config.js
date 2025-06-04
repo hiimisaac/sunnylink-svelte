b20aa88b0bd4f2b841f35354b60394d542cf357f
Isaac Barham <development@isaacbarham.com>
2025-06-03 21:15:25 -0400

    fix github pages spa

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,16 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: '/sunnylink-svelte'
-		}
-	}
+               adapter: adapter({
+                       // Use a 404.html fallback so Github Pages can serve
+                       // our single page application without server routing
+                       fallback: '404.html'
+               }),
+               paths: {
+                       base: '/sunnylink-svelte'
+               },
+               // Build purely static files. SSR is disabled at the route level
+       }
 };
 
 export default config;
