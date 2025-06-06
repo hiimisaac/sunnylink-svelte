2eaf0ec78c389b6306dd5d5d020f4c5c4dbfa175
Isaac Barham <development@isaacbarham.com>
2025-06-05 22:03:47 -0400

    Ok let's init it

--- src/routes/callback/+page.svelte
+++ src/routes/callback/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { PUBLIC_CALLBACK } from '$env/static/public';
+	import { PUBLIC_CALLBACK, PUBLIC_LOGTO_APP_ID, PUBLIC_LOGTO_ENDPOINT } from '$env/static/public';
+	import LogtoClient from '@logto/browser';
 	import { onMount } from 'svelte';
 
 	let { data } = $props();
@@ -8,7 +9,12 @@
 
 	onMount(async () => {
 		try {
-			await data.logtoClient?.handleSignInCallback(PUBLIC_CALLBACK);
+			const logtoClient = new LogtoClient({
+				endpoint: PUBLIC_LOGTO_ENDPOINT,
+				appId: PUBLIC_LOGTO_APP_ID
+			});
+
+			await logtoClient?.handleSignInCallback(PUBLIC_CALLBACK);
 
 			window.location.assign('/');
 		} catch (err) {
